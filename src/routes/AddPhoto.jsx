fb23af775f9002b50f3ba1fb190af3fca7a6ba86
Egidya <salsabilaggiya@gmail.com>
2022-12-29 20:35:31 +0700

    done

---
+++ src/routes/AddPhoto.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AddPhoto = () => {
+  const [imageUrl, setImageUrl] = useState("");
+  const [captions, setCaptions] = useState("");
+  const [secret, setSecret] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const addPhoto = (e) => {
+    e.preventDefault();
+    // TODO: answer here
+    const body = {
+      
+      createdAt: new Date().toString(),
+      updatedAt: new Date().toString(),
+      imageUrl, captions,
+      secret : secret,
+    }
+
+    fetch("http://localhost:3001/photos",{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body)
+
+    })
+    .then((response) => response.json())
+    .then((json) => {
+      navigate("/photos")
+    })
+  };
+
+  return (
+    <>
+      <div className="container">
+      {error && <div className="error-msg">{error}</div>}
+        <form className="add-form"  onSubmit={addPhoto}>
+          <label>
+            Image Url:
+            <input
+              className="add-input"
+              type="text"
+              data-testid="imageUrl"
+              value={imageUrl}
+              onChange={(e) => setImageUrl(e.target.value)}
+            />
+          </label>
+          <label>
+            Captions:
+            <input
+              className="add-input"
+              type="text"
+              data-testid="captions"
+              value={captions}
+              onChange={(e) => setCaptions(e.target.value)}
+            />
+          </label>
+          <label>
+            Secret:
+            <input
+              className="add-input"
+              type="text"
+              value={secret}
+              data-testid="secret"
+              onChange={(e) => setSecret(e.target.value)}
+            />
+          </label>
+          <input className="submit-btn" type="submit" value="Submit" data-testid="submit" />
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default AddPhoto;
